6ec4a30fecdf5c8b7bacc9b58ba0874982f66ba9
Marek Justyna <mjustyna@cs.put.poznan.pl>
2024-04-14 20:12:25 +0000

    Reproducibility improved

--- main_rna_tors.py
+++ main_rna_tors.py
@@ -12,9 +12,9 @@ from model_rna import RNAGNN
 from datasets import TorsionAnglesDataset
 
 def set_seed(seed):
+    torch.manual_seed(seed)
     torch.backends.cudnn.deterministic = True
     torch.backends.cudnn.benchmark = False
-    torch.manual_seed(seed)
     torch.cuda.manual_seed_all(seed)
     np.random.seed(seed)
     random.seed(seed)
