e0f1c1451ca556b919272abe394dc290ab3aef17
Marek Justyna <mjustyna@cs.put.poznan.pl>
2024-05-01 02:39:08 +0000

    New comparing algorithm added

--- tools/dcompare.sh
+++ tools/dcompare.sh
@@ -3,4 +3,4 @@ file1=$1
 file2=$2
 
 cd /home/macieka/2d-analysis/
-sudo docker-compose run --rm --name `cat /proc/sys/kernel/random/uuid` --entrypoint ./descs descs --execution-mode DESCRIPTORS_COMPARISON --file-path-of-first-descriptor $file1 --file-path-of-second-descriptor $file2 --molecule-type rna -cat BACKTRACKING_DRIVEN_FIRST_ALIGNMENT_ONLY --maximal-rmsd-of-central-elements-alignment 2.5 --maximal-rmsd-of-pair-of-aligned-duplexes 4.0 -maep 0.51 -aan /data/3d/desc-test/in-contact-residues-identification-based-on-c5prim-only.exp --output-directory /data/3d/desc-test/out
+sudo docker-compose run --rm --name `cat /proc/sys/kernel/random/uuid` --entrypoint ./descs descs --execution-mode DESCRIPTORS_COMPARISON --file-path-of-first-descriptor $file1 --file-path-of-second-descriptor $file2 --molecule-type rna -cat HUNGARIAN_METHOD_DRIVEN_FIRST_ALIGNMENT_ONLY_PARTIAL_SOLUTIONS_NOT_CONSIDERED --maximal-rmsd-of-central-elements-alignment 2.5 --maximal-rmsd-of-pair-of-aligned-duplexes 4.0 -maep 0.51 -aan /data/3d/desc-test/in-contact-residues-identification-based-on-c5prim-only.exp --output-directory /data/3d/desc-test/out
--- tools/run_dcompare.py
+++ tools/run_dcompare.py
@@ -11,10 +11,15 @@ FILES_PATH = '/data/3d/input_data/desc-pdbs/'
 def run_command(base1, base2):
     f1 = os.path.join(FILES_PATH, base1 + '.pdb')
     f2 = os.path.join(FILES_PATH, base2 + '.pdb')
-    # cmd = f"./dcompare.sh {f1} {f2} | grep 'are structurally similar' | tail -1 > out/{base1}-{base2}.txt"
-    cmd = f"./dcompare.sh {f1} {f2} | grep 'are structurally similar' > out/{base1}-{base2}.txt"
+    cmd = f"./dcompare.sh {f1} {f2} | grep 'are structurally similar' | tail -1 > out/{base1}-{base2}.txt"
+    # cmd = f"./dcompare.sh {f1} {f2} " #| grep 'are structurally similar'"
     if os.path.exists(f1) and os.path.exists(f2):
         os.system(cmd)
+        # get size of the output file
+        size = os.path.getsize(f'out/{base1}-{base2}.txt')
+        return size>0
+    else:
+        return False
 
 def read_list(list_file):
     with open(list_file) as f:
@@ -25,15 +30,53 @@ def read_list(list_file):
 def main():
     list_file = sys.argv[1]
     files = read_list(list_file)
-    # for f1 in tqdm(files[:9]):
-    #     for f2 in files[1:10]:
+    f_visited = {}
+    chain = {}
+
+    # if A is similart to B and B is similar to C, then A is similar to C
+    # algorithm:
+    # start with first A, compare with all other B
+    # if A is similar to B, then break. Add B to the chain of A
+    # start from B, compare with all other C. if C is similar to B, then break. Add C to the chain of A and B
+    
+    max = 20
+    for i in tqdm(range(len(files[:max-1]))):
+        if files[i] in f_visited:
+            continue
+        i1 = i
+        top_level_f1 = None
+        while i1 < len(files[:max-1]):
+            f1 = files[i1]
+            if top_level_f1 is None:
+                top_level_f1 = f1
+            f_visited[f1] = True
+
+            for i2 in range(i1+1, len(files[:max])):
+                f2 = files[i2]
+                if run_command(f1, f2):
+                    chain[top_level_f1] = chain.get(top_level_f1, [])
+                    chain[top_level_f1].append(f2)
+                    f_visited[f2] = True
+                    print(f1, f2, i1, i2)
+                    i1 = i2
+                    break
+            if i2 == len(files[:max])-1: # if chain is over
+                break
+            i1 += 1
+
+    # for i in tqdm(range(0, len(files[:max-1]))):
+    #     for j in range(i+1, len(files[:max])):
+    #         f1 = files[i]
+    #         f2 = files[j]
     #         run_command(f1, f2)
+
+    
     
     # run it in parallel with multiprocessing
-    pool = multiprocessing.Pool(processes=8)
-    for f1 in tqdm(files[:19]):
-        rcmd = partial(run_command, f1)
-        pool.map(rcmd, files[1:20])
+    # pool = multiprocessing.Pool(processes=8)
+    # for i, f1 in tqdm(enumerate(files[:10])):
+    #     rcmd = partial(run_command, f1)
+    #     pool.map(rcmd, files[i:11])
         
 
 
