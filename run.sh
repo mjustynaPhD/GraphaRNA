32284ca2e065074a7f6a797158e2832662d5ba82
Marek Justyna <mjustyna@cs.put.poznan.pl>
2025-03-11 09:54:11 +0100

    Updates

--- run.sh
+++ run.sh
@@ -8,4 +8,4 @@
 source ~/.bashrc
 conda activate gnn
 
-python main_rna_pdb_single.py --dataset RNA-bgsu-hl-cn --epoch=1 --batch_size=32 --dim=256 --n_layer=8 --lr=1e-5 --timesteps=5000 --mode=coarse-grain --knn=20 --wandb --lr-step=200
+python main_rna_pdb_single.py --dataset RNA-bgsu-hl-cn --epoch=1 --batch_size=32 --dim=256 --n_layer=6 --lr=1e-5 --timesteps=5000 --mode=coarse-grain --knn=20 --wandb --lr-step=200 --blocks=6
