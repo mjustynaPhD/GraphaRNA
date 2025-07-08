3d68fb63b9e71a0070ed58cd71edddd70a58c1d6
Marek Justyna <mjustyna@cs.put.poznan.pl>
2025-07-08 11:46:28 +0200

    Run for RNA language ablation (from scratch)

--- run_distributed.sh
+++ run_distributed.sh
@@ -11,4 +11,4 @@ conda activate gnn_test
 torchrun \
     --standalone \
     --nproc_per_node=4 \
-    src/grapharna/main_rna_pdb.py --dataset RNA-PDB-clean --epoch=400 --batch_size=16 --dim=256 --n_layer=6 --lr=1e-4 --timesteps=5000 --cutoff_l=0.5 --cutoff_g=1.6 --mode=coarse-grain --knn=20 --lr-step=50 --blocks=6 --load --wandb 
+    src/grapharna/main_rna_pdb.py --dataset RNA-PDB-clean --epoch=801 --batch_size=16 --dim=256 --n_layer=6 --lr=1e-4 --timesteps=5000 --cutoff_l=0.5 --cutoff_g=1.6 --mode=coarse-grain --knn=20 --lr-step=50 --blocks=6 --wandb 
